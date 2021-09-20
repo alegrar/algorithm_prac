33a67434e8af7fa0854add714042821df113e7cd
alegrar <47478054+alegrar@users.noreply.github.com>
2021-09-21 01:46:30 +0900

    Upload prac

--- programmers/미로 탈출.py
+++ programmers/미로 탈출.py
@@ -1,6 +1,24 @@
 def solution(n, start, end, roads, traps):
     answer = 0
-    print(n,start,end,roads,traps)
+    origin_dict = dict()
+    change_dict = dict()
+    for road in roads :
+        if road[0] in origin_dict.keys():
+            new = origin_dict[road[0]]
+            new.append([road[1], road[2]])
+            origin_dict[road[0]] = new
+        else :
+            origin_dict[road[0]] = [[road[1], road[2]]]
+    for key in origin_dict.keys():
+        for node in origin_dict[key]:
+            if node[0] not in change_dict.keys() : 
+                change_dict[node[0]] = [[key, node[1]]]
+            else :
+                new = change_dict[node[0]]
+                new.append([key,node[1]])
+                change_dict[node[0]] = new
+    print(origin_dict)
+    print(change_dict)
     return answer
 
 n = 3
