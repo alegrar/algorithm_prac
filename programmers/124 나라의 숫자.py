fd3bb84ca4631c4e46d2c3c923437f07d7e9b79c
alegrar <47478054+alegrar@users.noreply.github.com>
2021-11-04 00:29:15 +0900

    Upload prac

--- programmers/124 나라의 숫자.py
+++ programmers/124 나라의 숫자.py
@@ -1,7 +1,21 @@
 # 3진법 변환 코드
 
 def solution(n):
-    num = [1,2,4]
-
-    answer = ''
+    result = []
+    while n :
+        t = n % 3 # 1,2,3
+        if not t :
+            t = 3
+            n -= 1 # 나머지가 0이면 -1을 해서 나머지를 3으로 만들어야 함.
+        result.append(str(t))
+        n //= 3 # 3진법이므로 3보다 작으면 나머지로
+    for i in range(len(result)) :
+        if result[i] == '3' :
+            result[i] = '4'
+    answer = ''.join(result[::-1])
     return answer
+
+# 1,2,4
+n = 20
+# 242
+print(solution(n))
